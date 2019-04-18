d448d8b1451bd320487d99be712f5b30cdae4c5a
Pedro Giuliano Farina <pedrogfarina@gmail.com>
2019-04-18 13:03:37 -0300

    Adding project

---
+++ Traffic Unlocker/Traffic Unlocker/ViewController.swift
@@ -0,0 +1,90 @@
+//
+//  ViewController.swift
+//  Traffic Unlocker
+//
+//  Created by Aluno Mack on 18/04/19.
+//  Copyright © 2019 Aluno Mack. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
+class pickerController : UIViewController, UIPickerViewDelegate, UIPickerViewDataSource{
+    
+    @IBOutlet weak var picker: UIPickerView!
+    
+    var pickerData: [String] = [String]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        picker.delegate = self
+        picker.dataSource = self
+        
+        pickerData = ["Via interditada", "Desastre natural", "Acidente", "Obras"]
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // Number of columns of data
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    // The number of rows of data
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerData.count
+    }
+    
+    // The data to return fopr the row and component (column) that's being passed in
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerData[row]
+    }
+    
+}
+
+class pickerController2 : UIViewController, UIPickerViewDelegate, UIPickerViewDataSource{
+    
+    
+    @IBOutlet weak var picker: UIPickerView!
+    var pickerData: [String] = [String]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        picker.delegate = self
+        picker.dataSource = self
+        
+        pickerData = ["Camera ID 531", "Camera ID 372", "Camera ID 291"]
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // Number of columns of data
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    // The number of rows of data
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerData.count
+    }
+    
+    // The data to return fopr the row and component (column) that's being passed in
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerData[row]
+    }
+}
